a584f46e7d1aad56a92cee7158b4c63e4982bba5
nonlinebart <bartofzo@gmail.com>
2019-09-19 17:34:16 +0200

    initial commit

---
+++ Monoroutine.cs
@@ -0,0 +1,90 @@
+﻿/**
+ *
+ * Extension class for a Monobehaviour that allows for easy execution a single Coroutine at a time.
+ *
+ * Usage:
+ *
+ * using UnityMonoroutine;
+ *
+    // Fades in CanvasGroup and then fades it out again
+    canvasGroup.alpha = 0;
+    this.StartMonoroutine(canvasGroup.AlphaFade(1, 1),
+        () => this.StartMonoroutine(canvasGroup.AlphaFade(0, 1),
+        () => Debug.Log("Finished!")));
+ * 
+ * Example of such a routine would be a fade in/fadeout routine, where one can interrupt the other.
+ *
+ * Provides an optional callback that is garantueed to be called when the coroutine stops (either by finishing execution or by interruption)
+ * 
+ * (C) 2019 - Bart van de Sande / Nonline
+ * */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace UnityMonoroutine
+{
+    public static class Monoroutine
+    {
+        private static Dictionary<int, Tuple<Coroutine, Action>> routines = new Dictionary<int, Tuple<Coroutine, Action>>();
+
+        /// <summary>
+        /// Starts a Monoroutine on this Monobehaviour. Only a single Monoroutine will run at a given time for a Monobehaviour.
+        /// </summary>
+        /// <param name="monoBehaviour"></param>
+        /// <param name="coroutine">The coroutine to start</param>
+        /// <param name="callback">Optional callback to invoke when coroutine has finished executing or is interrupted</param>
+        public static void StartMonoroutine(this MonoBehaviour monoBehaviour, IEnumerator coroutine, Action callback = null)
+        {
+            int instanceId = monoBehaviour.GetInstanceID();
+
+            if (routines.TryGetValue(instanceId, out var runningRoutine))
+            {
+                // Stop already running 'mono' routine
+                monoBehaviour.StopCoroutine(runningRoutine.Item1);
+
+                // When stopped inbetween, fire stored callback
+                runningRoutine.Item2?.Invoke();
+            }
+
+            routines[instanceId] = new Tuple<Coroutine, Action>(monoBehaviour.StartCoroutine(WrapMonoroutine(coroutine, () =>
+            {
+                // Callback for when coroutine has fully completed
+                routines.Remove(instanceId);
+                callback?.Invoke();
+
+            })), callback);
+        }
+
+        /// <summary>
+        /// Stops the currently running Monoroutine for this Monobehaviour. 
+        /// </summary>
+        public static void StopMonoroutine(this MonoBehaviour monoBehaviour)
+        {
+            int instanceId = monoBehaviour.GetInstanceID();
+
+            if (routines.TryGetValue(instanceId, out var runningRoutine))
+            {
+                // Stop running 'mono' routine
+                monoBehaviour.StopCoroutine(runningRoutine.Item1);
+
+                // Stopped inbetween, fire stored callback
+                runningRoutine.Item2?.Invoke();
+
+                // Remove from collection
+                routines.Remove(instanceId);
+            }
+        }
+
+        private static IEnumerator WrapMonoroutine(IEnumerator coroutine, Action callback)
+        {
+            while (coroutine.MoveNext())
+            {
+                yield return coroutine.Current;
+            }
+            callback();
+        }
+    }
+}
---
+++ Monoroutines.cs
@@ -0,0 +1,33 @@
+﻿/**
+ *
+ * Handy collection of coroutines that can be used as monoroutines
+ *
+ *
+ * (C) 2019 - Bart van de Sande / Nonline
+ * 
+ **/
+
+using System.Collections;
+using UnityEngine;
+
+namespace UnityMonoroutine
+{
+    public static class Monoroutines
+    {
+        /// <summary>
+        /// Starts at the current alpha value of the canvas group and fades to targetAlpha
+        /// </summary>
+        public static IEnumerator AlphaFade(this CanvasGroup canvasGroup, float targetAlpha, float d = 0.33f)
+        {
+            float t = 0;
+            float startAlpha = canvasGroup.alpha;
+            while (t < d)
+            {
+                t += Time.deltaTime;
+                canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, t / d);
+                yield return null;
+            }
+            canvasGroup.alpha = targetAlpha;
+        }
+    }
+}
